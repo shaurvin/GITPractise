7fcc9f83dfcc68fc3f7c7279c4c215f2534211f2
shaurvin <shaurvincloud@gmail.com>
2025-01-28 12:57:02 +0530

    New files added

---
+++ GITPractise/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace GITPractise.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Gender { get; set; }
+    }
+}
